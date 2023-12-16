dc9aeae2b203883b3984479098942b11900b2b47
Sana-Najjar <najjarsana@hotmail.fr>
2023-12-16 17:42:55 +0100

    carousel finis

--- assets/script.js
+++ assets/script.js
@@ -16,39 +16,57 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// selection des flèches de navigation gauche et droite
 const btn_left = document.querySelector (".arrow_left");
-console.log(btn_left);
-const btn_right = document.querySelector ("arrow_right");
-console.log(btn_right);
+const btn_right = document.querySelector (".arrow_right");
+
+// selection des images et des textes
 let carrousel = document.querySelector ("#banner .banner-img");
 let text = document.querySelector ("#banner p");
+
+// récuperer les points
 let dots = document.getElementsByClassName ("dot");
-let i=0;
+
+// declaration du compteur
+let i=0; 
+
+// fléche à droite
 btn_right.addEventListener("click",function() {
-	if(i<slides.length) {
+	if(i<slides.length-1) { //avant l'arrivé à la derniere image
 		i++
-		carrousel.setAttribute('src',"./assets/images/slideshow/" + slides[i].image);
-		text.innerHTML=slides[i].tagLine;
-		dots[i-1].classList.remove("dot_selected");
-		dots[i].classList.add("dot_selected");
-	}else if (i>slides.length) {
-		carrousel.setAttribute('src',"./assets/images/slideshow/" + slides[0].image);
-		text.innerHTML=slides[0].tagLine;
-		dots[0].classList.add("dot_selected");
+		updateCarrousel(i) //appel à la fonction qui change l'image
+		updateDots()      // appel à la fonction qui modifie les points selectionnés
+	}else if(i==slides.length -1 ){ //lorsqu'on arrive à la dernière image
+		i=0;
+		updateCarrousel(i);
+		updateDots()
 	}
 })
+// fléche à gauche
 btn_left.addEventListener("click",function() {
-	if (i==0) {
-		carrousel.setAttribute('src',"./assets/images/slideshow/" + slides[slides.length].image);
-		text.innerHTML=slides[slides.length].tagLine;
+	if (i==0) { // lorqu'on est sur la première image
+		updateCarrousel(slides.length-1) // il affiche la dernière image
 		dots[0].classList.remove("dot_selected");
-		dots[slides.length].classList.add("dot_selected");
-		i=slides.length;
+        dots[slides.length-1].classList.add("dot_selected");
+		i=slides.length-1;
 	}else if (i<=slides.length-1){
-		carrousel.setAttribute('src',"./assets/images/slideshow/" + slides[i-1].image);
-		text.innerHTML=slides[i-1].tagLine;
+		updateCarrousel(i-1)
 		dots[i].classList.remove("dot_selected");
-		 dots[i-1].classList.add("dot_selected");
+		dots[i - 1].classList.add("dot_selected");
 		 i--;
 	}
 })
+
+// fonction pour changer l'image et son text
+function updateCarrousel(index) {
+	carrousel.setAttribute('src', "./assets/images/slideshow/" + slides[index].image);
+	text.innerHTML = slides[index].tagLine;
+  }
+
+  //fonction pouur changer les points selectionnés
+function updateDots() {
+	for (let dot of dots) {
+		dot.classList.remove("dot_selected");
+	}
+	dots[i].classList.add("dot_selected");
+}
